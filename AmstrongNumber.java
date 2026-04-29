4b660285f0482c9678eb2d136dce51ccd20b934c
AftabShaik <shaikaftab02@gmail.com>
2026-04-30 00:03:59 +0530

    solved amstrong number problem

---
+++ AmstrongNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AmstrongNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(isAmstrong(n));
+    }
+    static boolean isAmstrong(int num){
+        int ans = num;
+        int sum = 0;
+        while(num > 0) {
+            int rem = num % 10;
+            num = num / 10;
+            sum = sum + rem * rem * rem;
+        }
+        return sum == ans;
+    }
+}
