26d00f372a750d8e4637b591676bd48f7679c386
aftab91 <aftab91@github.com>
2026-05-22 02:11:17 +0530

    sum function update

--- sum.java
+++ sum.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 
 public class sum {
     public static void main(String[] args) {
+        int ans = sum();
+        System.out.println(ans);
+    }
+    public static int sum(){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int ans = sum(a,b);
-        System.out.println(ans);
-    }
-    public static int sum(int a, int b){
         return a+b;
     }
 }
