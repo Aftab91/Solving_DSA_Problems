a2f676d1cc3e1533d65226baa8653e36e003cbe6
aftab91 <shaikaftab02@gmail.com>
2026-05-24 00:41:48 +0530

    CountDigits

---
+++ CountDigits.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CountDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(countDigits(n));
+    }
+    static int countDigits(int n){
+        int count = 0;
+        while(n > 0){
+            n = n/10;
+            count++;
+        }
+        return count;
+    }
+}
